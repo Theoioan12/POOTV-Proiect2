4640c0113d4e26de2e7875b1e8209d60a55a3e5a
Theoioan12 <theodor_ioan.buliga@stud.acs.upb.ro>
2023-01-16 20:05:14 +0200

    Comentarii
    Am adaugat si comentariile.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 import com.fasterxml.jackson.databind.ObjectWriter;
-import functionalities.*;
+import functionalities.BannedCountries;
+import functionalities.DataBaseAdd;
+import functionalities.Filter;
+import functionalities.FilterFactory;
+import functionalities.Login;
+import functionalities.MovieData;
+import functionalities.Register;
 import inputmplementation.CredentialsInput;
 import inputmplementation.InputData;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,31 +26,34 @@ public final class Main {
     public static void main(final String[] args) throws IOException {
         File in = new File(args[0]);
         File out = new File(args[1]);
+
+        // reading the input data from Json
         ObjectMapper objectMapper = new ObjectMapper();
         InputData inputData = objectMapper.readValue(in,
                 InputData.class);
+
+        // output node
         ArrayNode output = objectMapper.createArrayNode();
 
         // a string reflecting the current page
         String currPage = "Homepage Neautentificat";
 
-        // an object to generate the output
+        // an object to generate the output, using singleton
         Output outputGenerator = Output.getInstance();
 
         // getting the number of commands
         int commandCount = inputData.getActions().size();
 
+        // subscribed string
         String subscribed = "-1";
         String previousPage = null;
 
         // current user
         UserData currUser = null;
 
+        // useful movie lists
         ArrayList<MovieData> currentMoviesList = new ArrayList<>();
-
-        // observable
         ArrayList<MovieData> availableMoviesList = new ArrayList<>();
-
         ArrayList<MovieData> filteredMovies = new ArrayList<>();
 
         // command loop
@@ -52,10 +61,12 @@ public final class Main {
             // change page command type
             if (inputData.getActions().get(i).getType().equals("change page")) {
                 if (currPage.equals("Homepage Neautentificat")) {
+
                     // login page
                     if (inputData.getActions().get(i).getPage().equals("login")) {
                         currPage = "login";
                         previousPage = "Homepage Neautentificat";
+
                         // register page
                     } else if (inputData.getActions().get(i).getPage().equals("register")) {
                         currPage = "register";
@@ -65,14 +76,19 @@ public final class Main {
                     } else {
                         outputGenerator.outputErrorGenerator(output, objectMapper);
                     }
+                    // logging out
                 } else if (inputData.getActions().get(i).getPage().equals("logout")) {
                     currPage = "Homepage Neautentificat";
+
+                    // movies page
                 } else if (inputData.getActions().get(i).getPage().equals("movies")
                         && currPage.equals("Homepage Autentificat")) {
                     currPage = "movies";
                     previousPage = "Homepage Autentificat";
                     outputGenerator.outputgenerator(output,
                             availableMoviesList, currUser, objectMapper);
+
+                    // error
                 } else {
                     outputGenerator.outputErrorGenerator(output, objectMapper);
                 }
@@ -81,21 +97,33 @@ public final class Main {
                 if (currPage.equals("login")
                         && inputData.getActions().get(i).getFeature().equals("login")) {
                     Login login = new Login();
+
                     if (login.login(inputData.getUsers(),
                             inputData.getActions().get(i).getCredentials()) != -1) {
                         currPage = "Homepage Autentificat";
+
                         int currUserIndex = login.login(inputData.getUsers(),
                                 inputData.getActions().get(i).getCredentials());
+
                         currUser =
                                 new UserData(inputData.getUsers().
                                         get(currUserIndex).getCredentials());
+
                         outputGenerator.outputgenerator(output,
                                 currentMoviesList, currUser, objectMapper);
+
                         availableMoviesList = new ArrayList<>();
+                        BannedCountries check = new BannedCountries();
+                        check.bannedCheck(inputData, currUser,
+                                availableMoviesList);
+
+                        // error
                     } else {
                         outputGenerator.outputErrorGenerator(output, objectMapper);
                         currPage = "Homepage Neautentificat";
                     }
+
+                    // register  action
                 } else if (currPage.equals("register")
                         && inputData.getActions().get(i).getFeature().equals("register")) {
                     CredentialsInput tmp = inputData.getActions().get(i).getCredentials();
@@ -110,13 +138,12 @@ public final class Main {
                     check.bannedCheck(inputData, currUser,
                             availableMoviesList);
 
+                    // filter action
                 } else if (currPage.equals("movies")
                         && inputData.getActions().get(i).getFeature().equals("filter")) {
-                    outputGenerator.outputgenerator(output,
-                            currentMoviesList, currUser, objectMapper);
                     int type = 2;
-                    if (inputData.getActions().get(i).getFilters().getContains() != null) {
 
+                    if (inputData.getActions().get(i).getFilters().getContains() != null) {
                         if (inputData.getActions().get(i).getFilters().
                                 getContains().getActors() != null) {
                             if (inputData.getActions().get(i).getFilters().
@@ -126,11 +153,15 @@ public final class Main {
                         } else {
                             type = 2;
                         }
+                        // using factory for the two strategies
                         Filter filter = FilterFactory.createFilter(type);
-                        filter.filter(inputData, currentMoviesList, i,
+                        filter.filter(inputData, availableMoviesList, i,
                                 filteredMovies);
                     }
+                    outputGenerator.outputgenerator(output,
+                            filteredMovies, currUser, objectMapper);
 
+                    // search function
                 } else if (currPage.equals("movies")
                         && inputData.getActions().get(i).getFeature().equals("search")) {
                      if (availableMoviesList.contains(inputData.
@@ -141,28 +172,39 @@ public final class Main {
                                 currentMoviesList, currUser, objectMapper);
                     }
 
+                     // subscribe function
                 } else if (currPage != "movies"
                         && inputData.getActions().get(i).getFeature().equals("subscribe")) {
                     subscribed = inputData.getActions().get(i).getSubscribedGenre();
+
+                    // database add
                 } else if (inputData.getActions().get(i).getType().equals("database")) {
                     if (inputData.getActions().get(i).getFeature().equals("add")) {
                         DataBaseAdd add = new DataBaseAdd();
                         add.add(inputData, inputData.getActions().get(i).getAddedMovie(), currUser);
-
                     }
+
+                    // error
                 } else {
                     outputGenerator.outputErrorGenerator(output, objectMapper);
                 }
+
+                // back error
             } else if (inputData.getActions().get(i).getType().equals("back")
                     && (currPage.equals("Homepage Autentificat"))) {
                 outputGenerator.outputErrorGenerator(output, objectMapper);
+
+                // working back
             }  else if (inputData.getActions().get(i).getType().equals("back")) {
                 currPage = previousPage;
             }
         }
+
+        // output in case of a premium user
         if (currUser.getCredentials().getAccountType().equals("premium")) {
             outputGenerator.finalOutput(output, currUser, objectMapper);
         }
+
         ObjectWriter objectWriter = objectMapper.writerWithDefaultPrettyPrinter();
         objectWriter.writeValue(out, output);
     }
